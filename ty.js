eea889839fc28bf3295cb0c4416739af34bb6b5b
6667799 <111183349+6667799@users.noreply.github.com>
2022-09-16 22:11:45 +0800

    Add files via upload

---
+++ ty.js
@@ -0,0 +1,23 @@
+
+   
+        
+  var zh = localStorage.getItem('message')
+  var mm = localStorage.getItem('message1')
+
+  if(   zh == "admin-y-qian-jj" ,   mm  == "webadmin-y-qian-jj"   ) 
+  
+      {
+       
+      }
+   else{
+    if( zh == "zhanghao1356" ,   mm  == "mima1356"){
+
+    }
+    else {
+      alert("请先登录账号密码")
+        window.location.href='./yh2.html';    
+  }
+   }
+
+
+   
